cf66ab90ad932346ab7073afb5186a971b9c645c
root <root@qomplex001.if.uff.int>
2013-02-10 20:46:43 -0200

    Adding config files for the cluster

---
+++ cluster/conf_cluster.sh
@@ -0,0 +1,39 @@
+pacman -Sy  sudo gvim git ifplugd netcfg  nfs-utils yp-tools ypbind-mt openssh yaourt libxml2 dbus ntp
+
+yaourt -S torque
+
+file="/etc/pacman.conf"
+word="archlinuxfr"
+
+cmd=$(grep -ci "$word" $file)
+
+if [ "$cmd" != "0" ]; then
+	echo "archlinufr already set"
+else
+        echo "[archlinuxfr]" >> $file
+        echo "Server = http://repo.archlinux.fr/\$arch" >> $file
+fi
+
+file="/etc/pacman.conf"
+word="archlinuxfr"
+cmd=$(grep -ci "$word" $file)
+
+#configuring nis
+echo "ypserver fiscomp" > /etc/yp.conf
+echo 'NISDOMAINNAME="fiscomp"' >  /etc/conf.d/nisdomainname
+cp nsswitch.conf /etc/nsswitch.conf
+systemctl enable ypbind.service
+
+#setting torque node
+echo "\$pbsserver      pinot  # note: this is the hostname of the headnode" > /var/spool/torque/mom_priv/config
+systemctl enable torque-node.service
+
+#setting the network
+systemctl enable sshd.service
+systemctl enable net-auto-wired.service
+ln -s /etc/network.d/examples/ethernet-dhcp /etc/network.d/
+
+#Enabling ntp to set the clock right 
+systemctl enable ntpd 
+timedatectl set-timezone America/Sao_Paulo
+cp ntp.conf /etc
