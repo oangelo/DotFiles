6be06ae01bde00417d1ec391c5f2a47d1d652743
oangelo <oangelo@gmail.com>
2021-09-26 12:10:07 -0300

    Adding stow to the packages to be installed

--- install_archlinux.sh
+++ install_archlinux.sh
@@ -1,5 +1,6 @@
 #vim packages
-sudo pacman -Sy colordiff bash-completion pkgfile powerline powerline-fonts zsh
+sudo pacman -Sy stow colordiff bash-completion pkgfile powerline powerline-fonts zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/supercrabtree/k ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/k
